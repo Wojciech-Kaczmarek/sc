06e7edc02e778d5c2b6939170c21d2d38290c6af
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-02-23 23:58:23 +0100

    day2

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('kiss')
+
+# x = 1  # integers
+# x = 1.1  # float
+# x = 1 + 2j  # a + bi = complex numbers)
+
+print(10 + 3)  # addition
+print(10 - 3)  # subtraction
+print(10 * 3)  # multiplication
+print(10 / 3)  # division
+print(10 // 3)  # if you wont integers
+print(10 % 3)  # modulus
+print(10 ** 3)  # exponent, left to the power of right
+
+x = 10
+x = x + 3  # statment
+x += 3  # augmented assignment operator
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log('hi mate');
+// console.log('hi mate');
